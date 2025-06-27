220467dbebb201deba729bdfaf52e7e7f222de6e
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-06-26 22:14:53 -0500

    feat: agregar estructura base del código

---
+++ inventarioProductosVentas.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+struct Producto{
+    string nombre;
+    float precio;
+};
+
+struct Venta{
+    int idVenta;
+    string producto;
+    int cantidad;
+    float precioTotal;
+};
+
+void registrarProducto(){
+
+}
+void listarProductos(){
+
+}
+
+void buscarProducto(){
+
+}
+
+void actualizarProducto(){
+
+}
+
+void eliminarProducto(){
+
+}
+
+void registrarVenta(){
+
+}
+
+void listarVentas(){
+
+}
+
+void calcularTotalVentas(){
+
+}
+
+void menu(){
+
+}
+
+int main (){
+    menu();
+    
+    return 0;
+}
