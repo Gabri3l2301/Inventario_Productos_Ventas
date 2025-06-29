31aad34155649f0ee1b9821e819a7f96da23e9e7
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-06-29 18:12:07 -0500

    feat: agregar función que muestre un menú con las opciones

--- inventarioProductosVentas.cpp
+++ inventarioProductosVentas.cpp
@@ -46,7 +46,56 @@ void calcularTotalVentas(){
 }
 
 void menu(){
+    SetConsoleOutputCP(CP_UTF8);
+    int opcion;
+    do{
+        cout << "===== MENÚ PRINCIPAL =====";
+        cout << "A: Registrar un nuevo producto\n";
+        cout << "B: Listar los productos registrados\n";
+        cout << "C: Buscar un producto por nombre\n";
+        cout << "D: Actualizar los datos de un producto\n";
+        cout << "E: Eliminar un producto\n";
+        cout << "F: Registrar una venta\n";
+        cout << "G: Listar las ventas realizadas\n";
+        cout << "H: Calcular el total de ventas realizadas\n";
+        cout << "S: Salir del programa\n";
+        cout << "Seleccione una opción: ";
+        cin >> opcion;
+        opcion = toupper(opcion);
 
+        switch (opcion){
+        case 'A':
+            registrarProducto();
+            break;
+        case 'B':
+            listarProductos();
+            break;
+        case 'C':
+            buscarProducto();
+            break;
+        case 'D':
+            actualizarProducto();
+            break;
+        case 'E':
+            eliminarProducto();
+            break;
+        case 'F':
+            registrarVenta();
+            break;
+        case 'G':
+            listarVentas();
+            break;
+        case 'H':
+            calcularTotalVentas();
+            break;
+        case 'S':
+            cout << "Saliendo del programa..." << endl;
+            break;
+        default:
+            cout << "Error, ingresa una opción válida..." << endl;
+            break;
+        }
+    }while (opcion != 'S');
 }
 
 int main (){
